5b13867ecb3fa09cb8ba386a851c74c6bdfd4c8c
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-12-06 11:44:30 +0100

    (feature): adds album data VV-31

---
+++ prisma/data/albums.ts
@@ -0,0 +1,112 @@
+export const albums = [
+  {
+    albumId: "1",
+    slug: "dont-get-too-close",
+    title: "Don't Get Too Close",
+    artist: "Skrillex",
+    genre: "Dubstep",
+    year: "2023",
+    imageUrl:
+      "https://t2.genius.com/unsafe/600x0/https%3A%2F%2Fimages.genius.com%2F215569f3f1b39c2e8e00b54825f79b63.1000x1000x1.png",
+    reviews: [],
+  },
+  {
+    albumId: "2",
+    slug: "come-to-daddy",
+    title: "Come To Daddy",
+    artist: "Aphex Twin",
+    genre: "d'n'b",
+    year: "1997",
+    imageUrl:
+      "https://t2.genius.com/unsafe/600x0/https%3A%2F%2Fimages.genius.com%2F23760f0bea626c01d9c21803c5a95fdb.1000x1000x1.jpg",
+    reviews: [],
+  },
+  {
+    albumId: "3",
+    slug: "moodymann",
+    title: "Moodymann",
+    artist: "Moodymann",
+    genre: "Electronic",
+    year: "2014",
+    imageUrl:
+      "https://t2.genius.com/unsafe/600x0/https%3A%2F%2Fimages.genius.com%2Fd1d5165632fc9722dbbe3c04aad3cf00.700x700x1.jpg",
+    reviews: [],
+  },
+  {
+    albumId: "4",
+    slug: "hengaillaan",
+    title: "Hengaillaan",
+    artist: "Kirka ",
+    genre: "Pop",
+    year: "1984",
+    imageUrl:
+      "https://t2.genius.com/unsafe/600x0/https%3A%2F%2Fimages.genius.com%2F98ccc513edd009e0fd24bb4c89f22e9f.600x600x1.jpg",
+    reviews: [],
+  },
+  {
+    albumId: "5",
+    slug: "actual-life-3",
+    title: "Actual Life 3 (January 1 - September 0 2022)",
+    artist: "Fred again..",
+    genre: "Electronic",
+    year: "2022",
+    imageUrl:
+      "https://t2.genius.com/unsafe/600x0/https%3A%2F%2Fimages.genius.com%2Fc7960dd5e55d23c9bc0b995f8c078b95.1000x1000x1.jpg",
+    reviews: [],
+  },
+  {
+    albumId: "6",
+    slug: "moment-ep",
+    title: "Moment EP",
+    artist: "Peggy Gou",
+    genre: "Electronic",
+    year: "2019",
+    imageUrl:
+      "https://t2.genius.com/unsafe/600x0/https%3A%2F%2Fimages.genius.com%2Fbc8128f69e2ccec833f1658613c33373.1000x1000x1.jpg",
+    reviews: [],
+  },
+  {
+    albumId: "7",
+    slug: "mind-fruit",
+    title: "Mind Fruit",
+    artist: "Opus III",
+    genre: "Electronic",
+    year: "1992",
+    imageUrl:
+      "https://t2.genius.com/unsafe/600x0/https%3A%2F%2Fimages.genius.com%2F0471777130ccd4a38135bac1dd360a52.1000x994x1.jpg",
+    reviews: [],
+  },
+  {
+    albumId: "8",
+    slug: "sticky-fingers",
+    title: "Sticky Fingers - Super Deluxe Edition",
+    artist: "The Rolling Stones",
+    genre: "Rock",
+    year: "2015",
+    imageUrl:
+      "https://t2.genius.com/unsafe/600x0/https%3A%2F%2Fimages.genius.com%2F8e6c199fe0c6112b62656b49fb669cf1.485x485x1.jpg",
+    reviews: [],
+  },
+  {
+    albumId: "9",
+    slug: "some-girls",
+    title: "Some Girls",
+    artist: "The Rolling Stones",
+    genre: "Rock",
+    year: "1978",
+    imageUrl:
+      "https://t2.genius.com/unsafe/600x0/https%3A%2F%2Fimages.genius.com%2F544b8780ee34f4d048a6eeec4c9d7d27.600x600x1.jpg",
+    reviews: [],
+  },
+  {
+    albumId: "10",
+    slug: "cave-world",
+    title: "Cave World",
+    artist: "Viagra Boys",
+    genre: "Rock",
+    year: "2023",
+    imageUrl:
+      "https://t2.genius.com/unsafe/600x0/https%3A%2F%2Fimages.genius.com%2F8eb4f76ab9d1b3325c43866a9289bcc4.1000x1000x1.jpg",
+    reviews: [],
+  },
+];
