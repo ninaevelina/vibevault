b8a81a6fd2d4692bd64a4aedca333586b96979bc
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-12-07 09:29:25 +0100

    (feature): adds first draft of base components (header, nav, footer)

---
+++ components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../Navbar";
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>VibeVault</h1>
+      <Navbar />
+    </header>
+  );
+};
---
+++ components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import "./navbar.scss";
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Nav item</li>
+        <li>Nav item</li>
+        <li>Nav item</li>
+      </ul>
+    </nav>
+  );
+};
