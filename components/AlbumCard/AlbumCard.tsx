816cd3c71a183bef5b4cc5f6e2a7bdc64da94819
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-12-08 13:18:03 +0100

    (feature): implements albumcard-component

--- components/AlbumCard/AlbumCard.tsx
+++ components/AlbumCard/AlbumCard.tsx
@@ -1,5 +1,7 @@
 import { Album } from "@prisma/client";
 import Link from "next/link";
+import "./albumcard.scss";
+import Image from "next/image";
 
 interface AlbumCardProps {
   album: Album;
@@ -7,11 +9,25 @@ interface AlbumCardProps {
 
 export default function AlbumCard({ album }: AlbumCardProps) {
   return (
-    <article>
-      <h2>{album.title}</h2>
-      <h3>{album.artist}</h3>
-      <label>{album.genre}</label>
-      <Link href={"/albums/" + album.slug}>View Album</Link>
+    <article className="album">
+      <div className="album__image">
+        <img
+          src={album.imageUrl}
+          alt={`Photo of ${album.title} by ${album.artist}`}
+        />
+        {/*<Image
+          src={album.imageUrl}
+          height={600}
+          width={600}
+          alt={`Image of ${album.title}`}
+  />*/}
+      </div>
+      <div className="album__details">
+        <h2>{album.title}</h2>
+        <h3>{album.artist}</h3>
+        <label>{album.genre}</label>
+        <Link href={"/albums/" + album.slug}>View Album</Link>
+      </div>
     </article>
   );
 }
