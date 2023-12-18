70eefd5be164e3ff80893e55bdc5f0087d5c2a1a
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-12-18 14:35:09 +0100

    (refactor): implements action instead of calling prisma-client

--- app/albums/page.tsx
+++ app/albums/page.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import prisma from "@/lib/db/prisma";
 import AlbumCard from "@/components/AlbumCard/AlbumCard";
+import { Album } from "@prisma/client";
+import { getAllAlbums } from "@/actions/albumActions";
+import { AlbumsFilter } from "@/components/AlbumsFilter/AlbumsFilter";
 
 export default async function Albums() {
-  const albums = await prisma.album.findMany();
-  console.log(albums);
+  const albums = await getAllAlbums();
 
   return (
-    <section className="grid-container">
-      {albums.map((album) => (
-        <AlbumCard album={album} key={album.id} />
-      ))}
-    </section>
+    <>
+      <AlbumsFilter />
+      <section className="grid-container">
+        {albums.map((album) => (
+          <AlbumCard album={album} key={album.id} />
+        ))}
+      </section>
+    </>
   );
 }
