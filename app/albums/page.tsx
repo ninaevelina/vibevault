f2038a4c59aa6dc541c1feaec9d358e6a30c47bd
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-12-19 14:11:35 +0100

    (refactor): adds albumsview-component to albums-page

--- app/albums/page.tsx
+++ app/albums/page.tsx
@@ -1,21 +1,10 @@
-import React, { useEffect, useMemo, useState } from "react";
-import prisma from "@/lib/db/prisma";
-import AlbumCard from "@/components/AlbumCard/AlbumCard";
-import { Album } from "@prisma/client";
-import { getAllAlbums } from "@/actions/albumActions";
-import { AlbumsFilter } from "@/components/AlbumsFilter/AlbumsFilter";
+import React from "react";
+import AlbumsView from "@/components/AlbumsView/AlbumsView";
 
 export default async function Albums() {
-  const albums = await getAllAlbums();
-
   return (
     <>
-      <AlbumsFilter />
-      {/*<section className="grid-container">
-        {albums.map((album) => (
-          <AlbumCard album={album} key={album.id} />
-        ))}
-        </section>*/}
+      <AlbumsView />
     </>
   );
 }
