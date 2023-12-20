1c045f0c4a4e6866bdcf9d73c12b8bb8f883fbf9
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-12-20 13:37:21 +0100

    (style): adds styles for search-component

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import "./search.scss";
 
 interface SearchProps {
   searchValue: string;
@@ -9,12 +10,13 @@ interface SearchProps {
 export const Search = ({ searchValue, handleSearch }: SearchProps) => {
   return (
     <section className="container-small">
-      <div>
+      <div className="search-container">
         <input
           type="text"
           value={searchValue}
-          placeholder="Search albums by title, artist, xxx"
+          placeholder="Search by title or artist"
           onChange={(e) => handleSearch(e.target.value)}
+          className="search-container__searchfield"
         />
       </div>
     </section>
