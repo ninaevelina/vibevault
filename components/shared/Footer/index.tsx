43ab7676e5563919ad792b25fbfc0ff2d486167c
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-12-29 12:19:46 +0100

    (feature): adds footer-component

--- components/shared/Footer/index.tsx
+++ components/shared/Footer/index.tsx
@@ -1,3 +1,34 @@
+import Link from "next/link";
+import "./footer.scss";
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="columns">
+        <div className="columns__column">Vibevault logo</div>
+        <div className="columns__column">
+          <ul>
+            <li>
+              <Link href={"/albums"} className="columns__column--link-item">
+                Albums
+              </Link>
+            </li>
+            <li>
+              <Link href={"/about"} className="columns__column--link-item">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href={"/contact"} className="columns__column--link-item">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="columns__column">Lorem ipsum dolor sit amet</div>
+      </div>
+      <div className="copyright">
+        <span>&copy; 2024 VibeVault</span>
+      </div>
+    </footer>
+  );
 };
