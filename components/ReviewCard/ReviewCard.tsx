4f96241aab8ece4f4233196489e5505c69d261c4
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-12-29 15:04:19 +0100

    (refactor): adds star-icon(s) to be rendered dynamically according to review rating

--- components/ReviewCard/ReviewCard.tsx
+++ components/ReviewCard/ReviewCard.tsx
@@ -1,14 +1,32 @@
 import { Review } from "@prisma/client";
 import "./reviewcard.scss";
+import { Star } from "../Icons/Star";
 
 interface ReviewCardProps {
   review: Review;
 }
 
+const renderRating = (rating: string) => {
+  const formattedRating = parseInt(rating, 10);
+  const stars = [];
+
+  if (!isNaN(formattedRating) && formattedRating > 0) {
+    for (let i = 0; i < formattedRating; i++) {
+      stars.push(
+        <span key={i}>
+          <Star />
+        </span>
+      );
+    }
+  }
+
+  return stars;
+};
+
 export default function ReviewCard({ review }: ReviewCardProps) {
   return (
     <article className="review">
-      <div className="review__rating">{review.rating} ⭐️</div>
+      <div className="review__rating">{renderRating(review.rating)}</div>
       <div className="review__content">{review.content}</div>
     </article>
   );
