ea14a4a3694503246b7af49795fa572bbead043d
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-12-30 12:13:26 +0100

    (refactor): adds action getAlbumBySlug

--- actions/albumActions.ts
+++ actions/albumActions.ts
@@ -1,4 +1,6 @@
+import prisma from "@/lib/db/prisma";
 import { Album } from "@prisma/client";
+import { cache } from "react";
 
 const baseUrl =
   "http://localhost:3000/api" ||
@@ -14,3 +16,9 @@ export const getAllAlbums = async (): Promise<Album[]> => {
   const results = await response.json();
   return results;
 };
+
+export const getAlbumBySlug = cache(async (slug: string): Promise<Album> => {
+  const response = await fetch(`${baseUrl}/albums/${slug}`);
+  const album = await response.json();
+  return album;
+});
