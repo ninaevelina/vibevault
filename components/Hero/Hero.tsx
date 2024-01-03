a8b217f430f1c34404a4ee8ee9010609b53d00f9
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-01-03 09:08:07 +0100

    (refactor): cleans up hero-component

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -4,11 +4,6 @@ export const Hero = () => {
     <section className="hero-section">
       <div className="hero-section__logo-container">
         <h1 className="hero-section__logo-container--logo">VIBEVAULT</h1>
-        <span className="hero-section__logo-container--description"></span>
-      </div>
-      <div className="hero-section__bottom-container">
-        <span></span>
-        <span></span>
       </div>
     </section>
   );
