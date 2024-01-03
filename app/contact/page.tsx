84848d31fa18928445fdfa5f737c4326f7de5291
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-01-03 09:41:14 +0100

    (refactor): adds containers to contactpage

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,10 @@
 export default function ContactPage() {
-  return <>ContactPage</>;
+  return (
+    <section className="container-small">
+      <h1>Contact</h1>
+      <div className="text-container">
+        <p>Lorem ipsum</p>
+      </div>
+    </section>
+  );
 }
