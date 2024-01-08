238cd1b28b78372f49e2cef6887b02a128362364
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-01-08 15:10:30 +0100

    (fix): adds missing forward slash VV-62

--- actions/albumActions.ts
+++ actions/albumActions.ts
@@ -3,11 +3,6 @@ import { cache } from "react";
 import { AlbumWithReviews } from "@/types/album";
 import { useBaseUrl } from "@/hooks/useBaseURL";
 
-/*const baseUrl =
-  "http://localhost:3000/api" ||
-  "https://vibevault-kappa.vercel.app/api" ||
-  "https://vibevault-git-vv-35-album-page-a-3b0b7c-ninas-projects-53ad124c.vercel.app/api";*/
-
 export const getAllAlbums = async (): Promise<Album[]> => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const baseURL = useBaseUrl();
@@ -18,13 +13,14 @@ export const getAllAlbums = async (): Promise<Album[]> => {
   console.log("albumdata", response.json);
 
   const results = await response.json();
+  console.log(results);
   return results;
 };
 
 export const getAlbumBySlug = cache(
   async (slug: string): Promise<AlbumWithReviews> => {
     const baseURL = useBaseUrl();
-    const response = await fetch(`${baseURL}api/albums/${slug}`);
+    const response = await fetch(`${baseURL}/api/albums/${slug}`);
     const data = (await response.json()) as AlbumWithReviews;
     console.log(data);
     return data;
