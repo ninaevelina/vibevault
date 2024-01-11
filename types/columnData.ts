59d7b0f54328516d18a11ca760b3424012d216a2
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-01-11 14:14:08 +0100

    (refactor): updates columndata

--- types/columnData.ts
+++ types/columnData.ts
@@ -16,24 +16,24 @@ export const columnsData: ColumnData[] = [
     link: "/albums",
     imageUrl: checkoutImg.src,
     heading: "View all albums",
-    text: "Lorem",
+    text: "",
   },
   {
     link: "/about",
     imageUrl: "/",
     heading: "What's our story?",
-    text: "Lorem",
+    text: "",
   },
   {
     link: "/contact",
     imageUrl: checkoutImg.src,
     heading: "Get in touch! 🖤 ",
-    text: "Lorem ipsum",
+    text: "",
   },
   {
     link: "/contact",
     imageUrl: imgBeyonce.src,
     heading: "Request an album",
-    text: "Yay",
+    text: "",
   },
 ];
